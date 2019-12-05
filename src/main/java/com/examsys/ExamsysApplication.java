9f90014ad37689cce3d72d39aec9ea79528e9a17
gjy gjy <xusysh@126.com>
2019-12-05 09:21:45 +0800

    update Auth

--- src/main/java/com/examsys/ExamsysApplication.java
+++ src/main/java/com/examsys/ExamsysApplication.java
@@ -2,11 +2,18 @@ package com.examsys;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class ExamsysApplication {
+public class ExamsysApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder applicationBuilder) {
+        return applicationBuilder.sources(ExamsysApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ExamsysApplication.class, args);
